42596f827774df36e8df76920ff58dda79dd4d4c
chibaf <chibaf@mac.com>
2021-09-11 16:52:16 +0900

    Add files via upload

--- read_csv.py
+++ read_csv.py
@@ -3,7 +3,6 @@ import sys
 
 with open(sys.argv[1]) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-#    line_count = 0
     for row in csv_reader:
       if len(row)==3:
         print([float(row[0]),float(row[1]),float(row[2])])
